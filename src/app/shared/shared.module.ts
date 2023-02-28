1620146215b9c01803e06b7a979d8fed76ff773f
dnyuhm <dylanmeuriot@gmail.com>
2023-02-28 11:13:26 +0100

    Commit 30 : Btn component ready to (re)use

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
+import { BtnComponent } from './components/btn/btn.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [TemplatesModule, IconsModule],
+  declarations: [BtnComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [TemplatesModule, IconsModule, BtnComponent],
 })
 export class SharedModule {}
